d46c9eebb82f1a6812926f144b8915c9e4435d95
geb <853934146@qq.com>
2024-05-14 21:55:59 +0800

    feat: update examole

--- scripts/mintFroimXfi.ts
+++ scripts/mintFroimXfi.ts
@@ -19,23 +19,40 @@ export async function run(provider: NetworkProvider, args: string[]) {
         .storeUint(144943127676063095663117939959419744222n, 128)
         .storeUint(0, 1).endCell();
 
-    await xFI.send(
-        provider.sender(),
-        {
-            value: toNano('3'),
-        },
-        {
-            $$type: "UserMint",
-            index: 123n,
-            to: Address.parse("EQAkZEqn5O4_yI3bCBzxpLEsO1Z10QSGDK5O4buL9nQrWI3p"),
-            amount: toNano(10000),
-            proof: cell1,
-            proof_length: 2n,
-            to_str: "UQAkZEqn5O4_yI3bCBzxpLEsO1Z10QSGDK5O4buL9nQrWNAs",
-        }
-    );
+    // await xFI.send(
+    //     provider.sender(),
+    //     {
+    //         value: toNano('3'),
+    //     },
+    //     {
+    //         $$type: "UserMint",
+    //         index: 123n,
+    //         to: Address.parse("EQAkZEqn5O4_yI3bCBzxpLEsO1Z10QSGDK5O4buL9nQrWI3p"),
+    //         amount: toNano(10000),
+    //         proof: cell1,
+    //         proof_length: 2n,
+    //         to_str: "UQAkZEqn5O4_yI3bCBzxpLEsO1Z10QSGDK5O4buL9nQrWNAs",
+    //     }
+    // );
+
+    // await xFI.send(
+    //     provider.sender(),
+    //     {
+    //         value: toNano('0.05'),
+    //     },
+    //     {
+    //         $$type: "MerkleAdmin",
+    //         value: Address.parse("EQDY-uI3LXl12N1cBduBMN911HM3MdPMijWxLnZPOpbMX6Fi")
+    //     }
+    // );
+
+    const mint_config = await xFI.getGetMintConfig()
+    console.log(mint_config)
 
     ui.write('Waiting for send...');
+
+    console.log(Address.parse("EQDY-uI3LXl12N1cBduBMN911HM3MdPMijWxLnZPOpbMX6Fi"))
+    console.log(mint_config.admin)
 
     ui.write('successfully!');
 }
